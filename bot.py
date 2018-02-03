6a51f13afe24cca64ac0024cf870c82cba780609
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-03 12:52:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,19 @@
 import discord
-import sys
 import os
 import io
+import traceback
+import sys
+import time
+import datetime
 import asyncio
+import random
+import aiohttp
+import pip
+import random
+import textwrap
+from contextlib import redirect_stdout
+from discord.ext import commands
+import json
 from discord.ext import commands
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('='),description="Zerkun Design's Discord bot.\n\nHelp Commands",owner_id=277981712989028353)
 
